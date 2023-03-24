e895de5bf49c5fb2ccb962984e1f13aa08e9681e
meoflynn <68336207+meoflynn@users.noreply.github.com>
2023-03-24 15:23:30 +0000

    Add class for creating netbox api object and unit test

---
+++ data_uploader/netbox_api/netbox_connection.py
@@ -0,0 +1,33 @@
+from data_uploader.exceptions.missing_mandatory_param_error import MissingMandatoryParamError
+import requests
+from pynetbox import api
+
+
+class NetboxApi:
+    """
+    Wraps a netbox connection as a context manager
+
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def api_object(netbox_url: str, token: str, cert=None):
+        if not netbox_url:
+            raise MissingMandatoryParamError(
+                "NetBox URL is required but not provided."
+            )
+        if not token:
+            raise MissingMandatoryParamError(
+                "A token is required but not provided."
+            )
+        session = requests.Session()
+        if not cert:
+            session.verify = False
+        else:
+            session.verify = cert
+
+        connection = api(url=netbox_url, token=token)
+        connection.http_session = session
+
+        return connection
